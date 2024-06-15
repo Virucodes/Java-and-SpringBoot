0f20126462eb18055d9a3ac10ee896ca1418ff8b
Virucodes <virajmane082@gmail.com>
2024-06-15 15:49:25 +0530

    Hello.java added

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello
+{
+     public static void main(String args[])
+     {
+        int num = 10_0_0_0_0_0_0;
+        //Converting integer into byte
+        int num2 = 457;
+        byte num3 = (byte)num2;
+        System.out.println(!(true||false));
+        System.out.println("Hello world!"+ num3);
+     }
+}
